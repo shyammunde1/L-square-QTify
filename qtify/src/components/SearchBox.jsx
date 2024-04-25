173747e28a0481a3df83ee9a52334659fc2a1cf1
samtech <shyammunde1@gmail.com>
2024-04-25 15:57:21 +0530

    add a placeholder in searchbox

--- qtify/src/components/SearchBox.jsx
+++ qtify/src/components/SearchBox.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import {ReactComponent as SearchIcon} from "../assets/search-icon.svg"
-import "./SearchBox.css"
+import { ReactComponent as SearchIcon } from "../assets/search-icon.svg";
+import "./SearchBox.css";
 
 const SearchBox = () => {
   return (
     <form className="container">
-      <input className="search" placeholder=""></input>
+      <input
+        className="search"
+        placeholder="Search a album of your choice"
+      ></input>
       <button className="search_icon" type="submit">
         <SearchIcon />
       </button>
