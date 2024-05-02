221fe5a9d0b35288e707dd097ebb3c139497a28b
samtech <shyammunde1@gmail.com>
2024-05-02 16:53:50 +0530

    footer add with player icon and progressbar

--- qtify/src/components/footer/Footer.jsx
+++ qtify/src/components/footer/Footer.jsx
@@ -1,20 +1,42 @@
 import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import React from "react";
 import Box from "@mui/material/Box";
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, Typography } from "@mui/material";
 import imgDemo from "../../assets/hero_headphones.png";
 
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
-    <div >
-      <Box className={styles.container} border={1} boxShadow="0px -2px 6px 0px #FFFFFF59" padding={2}>
+    <div>
+      <Box
+        className={styles.container}
+        border={1}
+        boxShadow="0px -2px 6px 0px #FFFFFF59"
+        padding={2}
+      >
         <img src={imgDemo} alt="testImg" />
-        <div>
-          <PlayArrowIcon />
-          <LinearProgress variant="determinate" value={30} />
+        <div className={styles.player}>
+          <div className={styles.playIcon}>
+            <PlayArrowIcon />
+          </div>
+          <div className={styles.progressbar}>
+            <Typography variant="body2" className={styles.label}>
+              0.30
+            </Typography>
+            <div className={styles.progress}>
+              <LinearProgress
+                variant="determinate"
+                value={10}
+                style={{ width: "600px" }}
+              />
+            </div>
+            <Typography variant="body2" className={styles.label} align="right">
+              3.38
+            </Typography>
+          </div>
         </div>
+        <div style={{ width: "100px" }}>{/* <h1>@samtech</h1> */}</div>
       </Box>
     </div>
   );
